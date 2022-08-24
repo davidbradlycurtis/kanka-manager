fe01b5e79a22816207f732d0b07a317c7d8654e7
David Curtis <davidbradlycurtis@gmail.com>
2022-08-23 20:35:13 -0500

    Add Dice

--- src/kankaclient/dice.py
+++ src/kankaclient/dice.py
@@ -7,9 +7,27 @@ from __future__ import absolute_import
 
 import logging
 import json
+from dataclasses import dataclass
+from typing import Any, Optional
+
+from dacite import from_dict
 
 from kankaclient.constants import BASE_URL, GET, POST, DELETE, PUT
-from kankaclient.base import BaseManager
+from kankaclient.base import BaseManager, Entity
+
+
+@dataclass
+class DiceRoll(Entity):
+
+    entry: Optional[Any]
+    image: Optional[Any]
+    image_full: Optional[Any]
+    image_thumb: Optional[Any]
+    is_private: bool
+    entity_id: int
+    character_id: Optional[int]
+    system: Optional[str]
+    rolls: Optional[list]
 
 class DiceRollAPI(BaseManager):
     """Kanka DiceRoll API"""
@@ -46,17 +64,24 @@ class DiceRollAPI(BaseManager):
         if self.dice_rolls:
             return self.dice_rolls
 
-        dice_rolls = list()
         response = self._request(url=GET_ALL_CREATE_SINGLE, request=GET)
 
         if not response.ok:
-            self.logger.error('Failed to retrieve dice_rolls from campaign %s', self.campaign.get('name'))
-            raise self.KankaException(response.text, response.status_code, message=response.reason)
+            self.logger.error(
+                "Failed to retrieve dice_rolls from campaign %s",
+                self.campaign.name,
+            )
+            raise self.KankaException(
+                response.text, response.status_code, message=response.reason
+            )
 
-        dice_rolls = json.loads(response.text).get('data')
         self.logger.debug(response.json())
+        if response.text:
+            self.dice_rolls = [
+                from_dict(data_class=DiceRoll, data=dice_roll) for dice_roll in json.loads(response.text).get("data")
+            ]
 
-        return dice_rolls
+        return self.dice_rolls
 
 
     def get(self, name_or_id: str or int) -> dict:
@@ -73,24 +98,28 @@ class DiceRollAPI(BaseManager):
             dice_roll: the requested dice_roll
         """
         dice_roll = None
-        if type(name_or_id) is int:
+        if isinstance(name_or_id, int):
             dice_roll = self.get_dice_roll_by_id(name_or_id)
         else:
-            dice_rolls = self.get()
+            dice_rolls = self.get_all()
             for _dice_roll in dice_rolls:
-                if _dice_roll.get('name') == name_or_id:
+                if _dice_roll.name == name_or_id:
                     dice_roll = _dice_roll
                     break
 
         if dice_roll is None:
-            raise self.KankaException(reason=f'Dice roll not found: {name_or_id}', code=404, message='Not Found')
+            raise self.raise_exception(
+                reason=f"Dice roll not found: {name_or_id}",
+                code=404,
+                message="Not Found",
+            )
 
         return dice_roll
 
 
-    def get_dice_roll_by_id(self, id: int) -> dict:
+    def get_dice_roll_by_id(self, id: int) -> DiceRoll:
         """
-        Retrieves the requested dice_roll from Kanka
+        Retrieves the requested dice roll from Kanka
 
         Args:
             id (int): the dice_roll id
@@ -104,13 +133,19 @@ class DiceRollAPI(BaseManager):
         response = self._request(url=GET_UPDATE_DELETE_SINGLE % id, request=GET)
 
         if not response.ok:
-            self.logger.error('Failed to retrieve dice_roll %s from campaign %s', id, self.campaign.get('name'))
-            raise self.KankaException(response.text, response.status_code, message=response.reason)
+            self.logger.error(
+                "Failed to retrieve dice_roll %s from campaign %s",
+                id,
+                self.campaign.name,
+            )
+            raise self.KankaException(
+                response.text, response.status_code, message=response.reason
+            )
 
-        dice_roll = json.loads(response.text).get('data')
+        dice_roll = json.loads(response.text).get("data")
         self.logger.debug(response.json())
 
-        return dice_roll
+        return from_dict(data_class=DiceRoll, data=dice_roll)
 
 
     def create(self, dice_roll: dict) -> dict:
@@ -129,7 +164,7 @@ class DiceRollAPI(BaseManager):
         response = self._request(url=GET_ALL_CREATE_SINGLE, request=POST, data=json.dumps(dice_roll))
 
         if not response.ok:
-            self.logger.error('Failed to create dice_roll %s in campaign %s', dice_roll.get('name', 'None'), self.campaign.get('name'))
+            self.logger.error('Failed to create dice_roll %s in campaign %s', dice_roll.get('name', None), self.campaign.name)
             raise self.KankaException(response.text, response.status_code, message=response.reason)
 
         dice_roll = json.loads(response.text).get('data')

8c70e2b9a9881b60b6ed1ab7a58acb09183e45c5
David Curtis <davidbradlycurtis@gmail.com>
2022-08-30 19:50:33 -0500

    Add items tests

--- src/kankaclient/items.py
+++ src/kankaclient/items.py
@@ -7,9 +7,30 @@ from __future__ import absolute_import
 
 import logging
 import json
+from dataclasses import dataclass
+from typing import Any, Optional
+
+from dacite import from_dict
 
 from kankaclient.constants import BASE_URL, GET, POST, DELETE, PUT
-from kankaclient.base import BaseManager
+from kankaclient.base import BaseManager, Entity
+
+
+@dataclass
+class Item(Entity):
+
+    entry: Optional[Any]
+    image: Optional[Any]
+    image_full: Optional[Any]
+    image_thumb: Optional[Any]
+    has_custom_image: bool
+    is_template: bool
+    entity_id: int
+    location_id: Optional[Any]
+    item_id: Optional[Any]
+    price: Optional[str]
+    size: Optional[str]
+    item_id: Optional[Any]
 
 class ItemAPI(BaseManager):
     """Kanka Item API"""
@@ -46,17 +67,24 @@ class ItemAPI(BaseManager):
         if self.items:
             return self.items
 
-        items = list()
         response = self._request(url=GET_ALL_CREATE_SINGLE, request=GET)
 
         if not response.ok:
-            self.logger.error('Failed to retrieve items from campaign %s', self.campaign.get('name'))
-            raise self.KankaException(response.text, response.status_code, message=response.reason)
+            self.logger.error(
+                "Failed to retrieve items from campaign %s",
+                self.campaign.name,
+            )
+            raise self.KankaException(
+                response.text, response.status_code, message=response.reason
+            )
 
-        items = json.loads(response.text).get('data')
         self.logger.debug(response.json())
+        if response.text:
+            self.items = [
+                from_dict(data_class=Item, data=item) for item in json.loads(response.text).get("data")
+            ]
 
-        return items
+        return self.items
 
 
     def get(self, name_or_id: str or int) -> dict:
@@ -73,22 +101,26 @@ class ItemAPI(BaseManager):
             item: the requested item
         """
         item = None
-        if type(name_or_id) is int:
+        if isinstance(name_or_id, int):
             item = self.get_item_by_id(name_or_id)
         else:
-            items = self.get()
+            items = self.get_all()
             for _item in items:
-                if _item.get('name') == name_or_id:
+                if _item.name == name_or_id:
                     item = _item
                     break
 
         if item is None:
-            raise self.KankaException(reason=f'Item not found: {name_or_id}', code=404, message='Not Found')
+            raise self.raise_exception(
+                reason=f"item not found: {name_or_id}",
+                code=404,
+                message="Not Found",
+            )
 
         return item
 
 
-    def get_item_by_id(self, id: int) -> dict:
+    def get_item_by_id(self, id: int) -> Item:
         """
         Retrieves the requested item from Kanka
 
@@ -104,16 +136,22 @@ class ItemAPI(BaseManager):
         response = self._request(url=GET_UPDATE_DELETE_SINGLE % id, request=GET)
 
         if not response.ok:
-            self.logger.error('Failed to retrieve item %s from campaign %s', id, self.campaign.get('name'))
-            raise self.KankaException(response.text, response.status_code, message=response.reason)
+            self.logger.error(
+                "Failed to retrieve item %s from campaign %s",
+                id,
+                self.campaign.name,
+            )
+            raise self.KankaException(
+                response.text, response.status_code, message=response.reason
+            )
 
-        item = json.loads(response.text).get('data')
+        item = json.loads(response.text).get("data")
         self.logger.debug(response.json())
 
-        return item
+        return from_dict(data_class=Item, data=item)
 
 
-    def create(self, item: dict) -> dict:
+    def create(self, item: dict) -> Item:
         """
         Creates the provided item in Kanka
 
@@ -126,16 +164,24 @@ class ItemAPI(BaseManager):
         Returns:
             item: the created item
         """
-        response = self._request(url=GET_ALL_CREATE_SINGLE, request=POST, data=json.dumps(item))
+        response = self._request(
+            url=GET_ALL_CREATE_SINGLE, request=POST, data=json.dumps(item)
+        )
 
         if not response.ok:
-            self.logger.error('Failed to create item %s in campaign %s', item.get('name', 'None'), self.campaign.get('name'))
-            raise self.KankaException(response.text, response.status_code, message=response.reason)
+            self.logger.error(
+                "Failed to create item %s in campaign %s",
+                item.get("name", "None"),
+                self.campaign.name,
+            )
+            raise self.KankaException(
+                response.text, response.status_code, message=response.reason
+            )
 
-        item = json.loads(response.text).get('data')
+        item = json.loads(response.text).get("data")
         self.logger.debug(response.json())
 
-        return item
+        return from_dict(data_class=Item, data=item)
 
 
     def update(self, item: dict) -> dict:
